3b5db93dcfcc799fb6dabc7832a853ac7e6f65e3
GabiruCwb <gab749@gmail.com>
2024-01-29 16:02:40 -0300

    update

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import AddFolder from "../assets/Add Folder.jpeg";
+import Config from "../assets/Config.jpeg";
+import OpenedFolder from "../assets/Opened folder.png";
 
 interface Props {
   rating: number;
 }
 
 const Emoji = ({ rating }: Props) => {
+  if (rating < 3) return null;
   return <div>Emoji</div>;
 };
 

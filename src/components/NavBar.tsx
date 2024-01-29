39f5186fcafbe1e0db6358d398fc17770c84633a
GabiruCwb <gab749@gmail.com>
2024-01-29 11:47:19 -0300

    update search

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,7 +5,7 @@ import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
       <Image src={Logo} />
       <SearchInput />
       <ColorModeSwitch />
